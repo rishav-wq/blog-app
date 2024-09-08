28350e8261b3785f361d8f5a157066e0519b4601
rishav-wq <r62599446@gmail.com>
2024-09-08 14:44:16 +0530

    final commit

---
+++ client/src/pages/categoryPages/CategoryPage.jsx
@@ -0,0 +1,44 @@
+// CategoryPage.jsx
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const CategoryPage = () => {
+  const { categoryName } = useParams();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPostsByCategory = async () => {
+      try {
+        const res = await axios.get(`http://localhost:5000/api/categories/posts/${categoryName}`);
+        setPosts(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching posts by category:", error);
+      }
+    };
+
+    fetchPostsByCategory();
+  }, [categoryName]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>Posts in Category: {categoryName}</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post._id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryPage;
